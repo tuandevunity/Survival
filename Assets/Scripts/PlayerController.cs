92eb9fcd7e15eab7f5184314b50fa7354dd8e010
tuandevunity <tuandixa999@gmail.com>
2025-02-12 17:32:47 +0700

    player control
    - add ui
    - add playercontroller scripts
    - import joystick pack
    - change background

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private FloatingJoystick floatingJoystick;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = floatingJoystick.Direction * moveSpeed;
+    }
+}
