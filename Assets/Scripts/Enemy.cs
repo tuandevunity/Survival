3f7d0fac30dd6214fc38fbcaf0be66092608f400
tuandevunity <tuandixa999@gmail.com>
2025-02-14 11:59:58 +0700

    refactor code
    - split enemy components, function
    - turn enemy into Prefabs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+public class Enemy : MonoBehaviour
+{
+    [Header("Components")]
+    private EnemyMovement movement;
+
+    [Header("Elements")]
+    private Player player;
+
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float playerDetectionRadius;
+
+    [Header("Spawn Squence Related")]
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+
+
+    [Header("Attack")]
+    [SerializeField] private int damage;
+    [SerializeField] private float attackFrequency;
+    private float attackDelay;
+    private float attackTimer;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem passAwayParticles;
+
+    [Header("Debug")]
+    [SerializeField] private bool gizmos;
+
+
+    private bool hasSpawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<EnemyMovement>();
+        player = FindFirstObjectByType<Player>();
+        if (player == null)
+        {
+            Debug.Log("No found player, auto destroying...");
+            Destroy(gameObject);
+        }
+
+        StartSpawnSequence();
+
+        attackDelay = 1f / attackFrequency;
+    }
+
+    private void Update()
+    {
+        if (!hasSpawned) return;
+
+
+
+        if (attackTimer >= attackDelay)
+        {
+            TryAttack();
+        }
+        else
+        {
+            Wait();
+        }
+    }
+
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer <= playerDetectionRadius)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log($"dealing {damage} to player");
+        attackTimer = 0;
+    }
+
+    private void Wait()
+    {
+        attackTimer += Time.deltaTime;
+    }
+
+    private void PassAway()
+    {
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+        Destroy(gameObject);
+    }
+
+
+    private void StartSpawnSequence()
+    {
+        SetEnemyVisibility(false);
+
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f).
+            setLoopPingPong(4).
+            setOnComplete(SpawnSequenceCompleted);
+    }
+
+    private void SpawnSequenceCompleted()
+    {
+        SetEnemyVisibility(true);
+        hasSpawned = true;
+
+        movement.StorePlayer(player);
+    }
+
+    private void SetEnemyVisibility(bool visibility)
+    {
+        renderer.enabled = visibility;
+        spawnIndicator.enabled = !visibility;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!gizmos) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
+    }
+    // Update is called once per frame
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,65 +7,19 @@ public class EnemyMovement : MonoBehaviour
     [Header("Elements")]
     private Player player;
 
-    [Header("Spawn Squence Related")]
-    [SerializeField] private SpriteRenderer renderer;
-    [SerializeField] private SpriteRenderer spawnIndicator;
-
-    private bool hasSpawned;
-
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadius;
-
-    [Header("Attack")]
-    [SerializeField] private int damage;
-    [SerializeField] private float attackFrequency;
-    private float attackDelay;
-    private float attackTimer;
-
-    [Header("Effects")]
-    [SerializeField] private ParticleSystem passAwayParticles;
-
-    [Header("Debug")]
-    [SerializeField] private bool gizmos;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = FindFirstObjectByType<Player>();
-        if (player == null)
-        {
-            Debug.Log("No found player, auto destroying...");
-            Destroy(gameObject);
-        }
-
-        renderer.enabled = false;
-        spawnIndicator.enabled = true;
-
-        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
-        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f).
-            setLoopPingPong(4).
-            setOnComplete(SpawnSequenceCompleted);
-
-        attackDelay = 1f / attackFrequency;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (!hasSpawned) return;
+        if (player == null) return;
+        FollowPlayer();       
+    }
 
-        FollowPlayer();
-
-        if (attackTimer >= attackDelay)
-        {
-            TryAttack();
-        }
-        else
-        {
-            Wait();
-        }
+    public void StorePlayer(Player _player)
+    {
+        player = _player;
     }
 
     private void FollowPlayer()
@@ -76,44 +30,5 @@ public class EnemyMovement : MonoBehaviour
         transform.position = targetPosition;
     }
 
-    private void TryAttack()
-    {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-        if (distanceToPlayer <= playerDetectionRadius)
-        {
-            Attack();
-        }
-    }
-
-    private void Attack()
-    {
-        Debug.Log($"dealing {damage} to player");
-        attackTimer = 0;
-    }
-
-    private void Wait()
-    {
-        attackTimer += Time.deltaTime;
-    }
-
-    private void PassAway()
-    {
-        passAwayParticles.transform.SetParent(null);
-        passAwayParticles.Play();
-        Destroy(gameObject);
-    }
-
-    private void SpawnSequenceCompleted()
-    {
-        renderer.enabled = true;
-        spawnIndicator.enabled = false;
-        hasSpawned = true;
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (!gizmos) return; 
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
-    }
+   
 }
