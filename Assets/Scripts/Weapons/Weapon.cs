64dbd3ff132c2571fc43b0866fb0b134a88bfe38
tuandevunity <tuandixa999@gmail.com>
2025-02-16 12:15:16 +0700

    automatically aim for near target

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("Settings")]
+    [SerializeField] private float range;
+    [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private float aimLerp;
     void Start()
     {
         
@@ -13,9 +17,53 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Transform enemy = null;
-        enemy = FindObjectsByType<Enemy>(FindObjectsInactive.Exclude, FindObjectsSortMode.None)[0].transform;
-        transform.up = (enemy.position - transform.position).normalized;
-        Debug.Log(transform.up);
+        AutoAim();
+    }
+
+    private void AutoAim()
+    {
+        Enemy closestEnemy = GetClosestEnemy();
+        Vector3 targetUpVector = Vector3.up;
+
+
+        if (closestEnemy != null)
+        {
+            targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+        }
+
+        transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
+
+
+    }
+
+    private Enemy GetClosestEnemy()
+    {
+        Enemy closestEnemy = null;
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyMask);
+
+        int enemyCount = enemies.Length;
+        float minDistance = range;
+
+        if (enemyCount <= 0) return null;
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Enemy enemyChecked = enemies[i].GetComponent<Enemy>();
+            float distanceToEnemy = Vector2.Distance(transform.position, enemyChecked.transform.position);
+
+            if (distanceToEnemy < minDistance)
+            {
+                closestEnemy = enemyChecked;
+                minDistance = distanceToEnemy;
+            }
+
+        }
+        return closestEnemy;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
